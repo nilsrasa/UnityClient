a94ae3a0cb2ef686803decb9fda7a520efa2ba1a
AntonaiNestor <antonai_nestor@hotmail.com>
2018-07-04 18:05:59 +0200

    Microphone Usage for Ricoh Theta
    Enabled the usage of the microphone attached to the 360 Ricoh Theta camera.

--- Assets/Scripts/ThetaWebcamStream.cs
+++ Assets/Scripts/ThetaWebcamStream.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 //Connects to the Theta S UVC stiching software and outputs a square image
 public class ThetaWebcamStream : MonoBehaviour
@@ -8,6 +9,16 @@ public class ThetaWebcamStream : MonoBehaviour
     [SerializeField] private bool _useDummyImage;
     [SerializeField] private Texture _dummyImage;
     [SerializeField] private Texture _text;
+    [SerializeField] private AudioClip defaUltTestClip;
+
+    private bool microphoneEnabled = false;
+    private AudioSource audiosource;
+
+
+    public void Awake()
+    {
+        audiosource = gameObject.GetComponent<AudioSource>();
+    }
 
     public void StartStream()
     {
@@ -20,19 +31,59 @@ public class ThetaWebcamStream : MonoBehaviour
        
         
        WebCamTexture mycam = new WebCamTexture();
+        //I think this name is wrong here. Only when RICOH THETA S is part of the devices list the camera actually outputs information
         string camName = "THETA UVC FullHD Blender";
         mycam.deviceName = camName;
         _primarySphere.sharedMaterial.mainTexture = mycam;
         _text = mycam;
-        if (!mycam.isPlaying)
+        mycam.Play();
+
+        //Audio stream from microphone
+
+        
+        if (audiosource)
         {
-            Debug.Log("Playing cam");
-            mycam.Play();
+            Debug.Log("Found Audiosource");
+
+            //Create soundclip from microphone, by making a small recording of  1 second duration.
+            //Name for the microphone is either manually the one below or : Microphone.Devices[0] . However this seems a safer choice in the case that there is another microphone source in that slot.
+           // AudioClip micClip = Microphone.Start("Microphone (RICOH THETA S)", true, 1, 44100);
+            audiosource.clip =  defaUltTestClip;//micClip ? micClip :
+            audiosource.loop = true;
+            audiosource.Play();
+            microphoneEnabled = true;
         }
-        foreach (var variable in WebCamTexture.devices)
+        else
         {
-         
-            Debug.Log(variable.name);
+            Debug.Log("No audiosource attached to gameobject");
         }
+
+
+        //Debug show microphone devices
+        foreach (var mic in Microphone.devices)
+        {
+            Debug.Log(mic);
+        }
+
+        //Debug show the devices to ensure that theta camera is connected
+        //
+        //foreach (var variable in WebCamTexture.devices)
+        //{
+        //    Debug.Log(variable.name);
+        //}
     }
+
+    public void Update()
+    {
+
+        //mute/unmute microphone  microphone with hotkey S
+        if (Input.GetKeyDown("s"))
+        {
+            audiosource.mute = microphoneEnabled;
+            microphoneEnabled = !microphoneEnabled;
+        }
+
+
+    }
+
 }
