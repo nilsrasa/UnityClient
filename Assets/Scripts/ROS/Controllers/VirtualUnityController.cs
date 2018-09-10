1c411fe58d6dfa9d95f20fb56b4b18efdf23b1ec
AntonaiNestor <antonai_nestor@hotmail.com>
2018-09-10 17:43:30 +0200

    Virtual Unity Controller update
    -Added Joystick implementation for the new controller.
    -Added screen shakes to improve realism
    -Fixed rotation issues during collisions with obstacles. The robot will now rotate back to initial position
    -Added drag during collision with obstacle.
    -Added simple sensors that the distance to colliding object and its type.

--- Assets/Scripts/ROS/Controllers/VirtualUnityController.cs
+++ Assets/Scripts/ROS/Controllers/VirtualUnityController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using EZCameraShake;
 using UnityEngine;
 
 
@@ -15,7 +16,7 @@ public class VirtualUnityController : MonoBehaviour {
     [SerializeField] private float MaxLinearVelocity = 0.4f;
     [SerializeField] private float MinLinearVelocity = 0.05f;
     [SerializeField] private float MaxAngularVelocity = 0.8f;
-    [SerializeField] private float BackwardsVelocity = 0.1f;
+    [SerializeField] private float BackwardsVelocity = 0.3f;
 
     [HideInInspector] public bool IsConnected = false;
     private Rigidbody VirtualBot;
@@ -32,6 +33,14 @@ public class VirtualUnityController : MonoBehaviour {
 
     private Vector2 InitRange;
     private Vector2 NewRange;
+    private float TargetRotLow;
+    private float TargetRotHigh;
+    private float TargetRot;
+    private float InitRot;
+    private bool InitialShake;
+    bool balancing = false;
+    private float val = 0.0f;
+    private float rotateSpeed = 0.01f;
 
 
     void Awake()
@@ -45,16 +54,124 @@ public class VirtualUnityController : MonoBehaviour {
         InitRange = new Vector2(UpperDeadZoneLimit, MaxVelocityHorizon);
         NewRange = new Vector2(0, MaxLinearVelocity);
         VirtualBot = this.gameObject.GetComponent<Rigidbody>();
+        TargetRotLow = 0.0f;
+        TargetRotHigh = 359.0f;
+        InitRot =  0.0f;
+        InitialShake = false;
     }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+	{
+
+        //UGLY BUT WORKS -> PUT IN FUNCTION FOR OPTIMIZING MAYBE 
+	    float CurrentAngle = gameObject.transform.eulerAngles.x;
+	    //Debug.Log(CurrentAngle);
+
+        if (CurrentAngle > 2.0f && CurrentAngle < 10.0f && !balancing)
+	    {
+	        balancing = true;
+	        val = 0.0f;
+	        InitRot = CurrentAngle;
+	        TargetRot = TargetRotLow;
+	    }
+	    else if (CurrentAngle < 358.0f && CurrentAngle > 340.0f && !balancing)
+	    {
+	        balancing = true;
+	        val = 0.0f;
+	        InitRot = CurrentAngle;
+	        TargetRot = TargetRotHigh;
+	    }
+
+        if (balancing)
+	    {
+	        val = val + rotateSpeed;
+	        float angle = Mathf.Lerp(InitRot, TargetRot, val);
+            //rotate only x axis, ugly but it works
+	        transform.rotation = Quaternion.Euler(angle, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+
+	        if (val >= 1.0f)
+	        {
+	            balancing = false;
+	        }
+        }
+
+    }
 
     public void JoystickCommand(Vector2 input)
     {
-       //Filter the joystick input appropriately
+       
+        Vector2 movement = new Vector2(input.y, input.x);
+
+        if (!movement.Equals(new Vector2(0, 0)) && !InitialShake)
+        {
+            CameraShaker.Instance.ShakeOnce(1f, 1f, 0.2f, 0.2f);
+            InitialShake = true;
+        }
+
+
+        if (movement.Equals(new Vector2(0, 0)))
+        {
+            InitialShake = false;
+        }
+    
+        // Debug.Log(movement.x);
+        if (movement.x > MaxLinearVelocity) movement.x = MaxLinearVelocity;
+        else if (movement.x < 0) movement.x = -BackwardsVelocity;
+
+        if (Mathf.Abs(movement.y) > MaxAngularVelocity)
+        {
+            if (movement.y < 0) movement.y = -MaxAngularVelocity;
+            else movement.y = MaxAngularVelocity;
+        }
+       
+        Vector3 velocity = this.gameObject.transform.right * movement.x;
+        
+        VirtualBot.AddForce(velocity, ForceMode.VelocityChange);
+        this.gameObject.transform.Rotate(this.gameObject.transform.up, Mathf.Rad2Deg * Time.deltaTime * movement.y, Space.World);
+    }
+
+    void BalanceRobot(Vector2 input)
+    {
+       
+
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            CameraShaker.Instance.ShakeOnce(2f, 2f, 0.5f, 0.5f);
+            VirtualBot.drag = 10.0f;
+        }
+        
+    }
+
+    //void OnCollisionStay(Collision collision)
+    //{
+       
+    //    if (collision.gameObject.CompareTag("Obstacle"))
+    //    {
+    //        VirtualBot.drag = 20.0f;
+    //    }
+    //    else
+    //    {
+    //        VirtualBot.drag = 0.0f;
+    //    }
+      
+    //    //CameraShaker.Instance.ShakeOnce(2f, 2f, 0.5f, 0.5f);
+    //    //Debug.Log("Collided!");
+    //}
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            CameraShaker.Instance.ShakeOnce(2f, 2f, 0.5f, 0.5f);
+            VirtualBot.drag = 0.0f;
+        }
 
     }
 
@@ -67,16 +184,15 @@ public class VirtualUnityController : MonoBehaviour {
         if (!InsideDeadZone(command.x, command.y))
         {
             //normalize speed and send data
+            Debug.Log(FilterLinearVelocity(command.x));
             VirtualBot.velocity = this.gameObject.transform.right * FilterLinearVelocity(command.x);
-            VirtualBot.angularVelocity = this.gameObject.transform.up * FilterAngularVelocity(command.y);
+            //VirtualBot.angularVelocity = this.gameObject.transform.up; // * FilterAngularVelocity(command.y);
+            this.gameObject.transform.Rotate(this.gameObject.transform.up, Mathf.Rad2Deg *Time.deltaTime*FilterAngularVelocity(command.y), Space.World);
         }
         else
         {
           StopRobot();
         }
-        
-       
-        //angular rotation goes here
     }
 
     private float FilterLinearVelocity(float vel)
